cf507efa2c0a2d5c474fbd88553a7370cfa9f87e
yangsiyuan <yangsiyuan@meiqia.com>
2019-12-13 16:35:27 +0800

    fix

--- mysql.go
+++ mysql.go
@@ -15,12 +15,17 @@ func InitGorm() {
 	gormInstance = &Gorm{make(map[string]*gorm.DB)}
 	mainDb, err := gorm.Open("mysql", MConfig.MysqlSet["main"].Dsn)
 	errExit(err)
+	mainDb.DB().SetMaxIdleConns(MConfig.MysqlSet["main"].MaxIdle)
+	mainDb.DB().SetMaxOpenConns(MConfig.MysqlSet["main"].MaxOpen)
 
 	gormInstance.gormSet["main"] = mainDb
 
 	if MConfig.MysqlSet["salve"] != nil {
 		slaveDb, err := gorm.Open("mysql", MConfig.MysqlSet["slave"].Dsn)
 		errExit(err)
+
+		slaveDb.DB().SetMaxIdleConns(MConfig.MysqlSet["slave"].MaxIdle)
+		slaveDb.DB().SetMaxOpenConns(MConfig.MysqlSet["slave"].MaxOpen)
 
 		gormInstance.gormSet["slave"] = slaveDb
 	}
